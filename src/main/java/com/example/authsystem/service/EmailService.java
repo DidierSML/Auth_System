b558590c3bdfb2867b9a2ec673b87e7a1a9f7635
DidierSML <didiersmaldonad@gmail.com>
2025-04-29 17:42:34 -0500

    docs: Improve README with test data

--- src/main/java/com/example/authsystem/service/EmailService.java
+++ src/main/java/com/example/authsystem/service/EmailService.java
@@ -1,19 +1,21 @@
 package com.example.authsystem.service;
 
-import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
 @Service
-@AllArgsConstructor
 public class EmailService {
 
     private final JavaMailSender javaMailSender;
 
     @Value("${mail.from}")
     private String mailFrom;
+
+    public EmailService(JavaMailSender javaMailSender) {
+        this.javaMailSender = javaMailSender;
+    }
 
     public void sendEmail (String recipient, String subject, String body){
 
